ee1d0c510ea6f6bf6260e2df820082aae6a0bf43
Maxime404 <galissairemaxime24@gmail.com>
2021-01-14 22:45:52 +0100

    WIP - Finished managing player's behavior

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const inquirer = require('inquirer')
-const { isEmpty } = require('lodash')
 const _ = require('lodash')
 
 class Ailumette {
@@ -12,12 +11,6 @@ class Ailumette {
         [' ', '|', '|', "|", '|', '|', ' '],
         ['|', '|', '|', '|', '|', '|', '|']
     ]
-    boardCopy = [
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 1, 1, 1, 1, 1]
-    ]
     questionLine = [{
         type: 'input',
         name: 'line',
@@ -29,21 +22,24 @@ class Ailumette {
         message: "Matches:",
     }]
 
-    constructor() {
+    constructor() { }
+
+    start() {
         console.log('$ node ailumette\n')
         this.displayMap()
+        console.log('\n' + (this.turn ? 'Your turn:' : 'AI’s turn...'))
+        this.round()
     }
 
     async round() {
         if (this.turn) {
-            console.log('Your turn:')
-
             while (this.line === 0) {
                 const line = await this.setLine()
                 if (this.isValidEntry(line, 'line')) {
                     this.line = line
                 }
             }
+
             while (this.matches === 0) {
                 const matches = await this.setMatches()
                 if (this.isValidEntry(matches, 'matches')) {
@@ -51,19 +47,18 @@ class Ailumette {
                 }
             }
 
+            this.updateBoard()
         } else {
-            console.log('AI’s turn...')
-
+            //aiPlays()
 
         }
-        this.turn = this.turn ? 0 : 1
+        //this.turn = this.turn ? 0 : 1
     }
 
     setLine() {
         return new Promise(resolve => {
             inquirer.prompt(this.questionLine)
                 .then(answerLine => {
-                    console.log(answerLine.line)
                     resolve(parseInt(answerLine.line))
                 })
         })
@@ -78,17 +73,43 @@ class Ailumette {
         })
     }
 
-    update() {
+    updateBoard() {
+        this.board[this.line - 1] = this.board[this.line - 1].fill(' ', (this.board[this.line - 1].lastIndexOf('|') + 1) - this.matches)
+
+        let allMatchesCount = 0
+        _.each(this.board, (line) => {
+            allMatchesCount += _.countBy(line)["|"] || 0
+        })
+
+        console.log('allMatchesCount', allMatchesCount)
+
+        console.log(`${this.turn ? 'Player' : 'AI'} removed ${this.matches} match(es) from line ${this.line}`)
+
         this.displayMap()
+
+        if (allMatchesCount === 0) {
+            console.log(this.turn ? 'I lost.. snif.. but I’ll get you next time!!' : 'You lost, too bad..')
+        } else {
+            this.line = this.matches = 0
+            console.log('\n' + (this.turn ? 'Your turn:' : 'AI’s turn...'))
+            this.round()
+        }
     }
 
     isValidEntry(number, type) {
-        if (number < 0 || typeof number !== 'number' || !number) {
+        const matchesCount = type === 'line'
+            ? _.countBy(this.board[number - 1])["|"] || 0
+            : _.countBy(this.board[this.line - 1])["|"] || 0
+
+        if (number < 0 || typeof number !== 'number') {
             console.log('Error: invalid input (positive number expected)')
             return false
         } else if (type === 'line') {
             if (number === 0 || number > 4) {
                 console.log('Error: this line is out of range')
+                return false
+            } else if (matchesCount === 0) {
+                console.log('Error: there’s no more matches here')
                 return false
             }
         } else if (type === 'matches') {
@@ -98,11 +119,7 @@ class Ailumette {
             } else if (number > 3) {
                 console.log('Error: the maximum number of matches is 3')
                 return false
-            }
-            const matchesCount = _.countBy(this.board[this.line])["|"]
-            console.log('matchesCount :', matchesCount)
-
-            if (number > matchesCount) {
+            } else if (number > matchesCount) {
                 console.log('Error: not enough matches on this line')
                 return false
             }
@@ -115,14 +132,10 @@ class Ailumette {
         _.each(this.board, (line) => {
             console.log('*' + line.join('') + '*')
         })
-        console.log('*********\n')
-    }
-
-    getValues() {
-        console.log(this.line, this.matches)
+        console.log('*********')
     }
 }
 
 const ailumette = new Ailumette()
 
-ailumette.round()
+ailumette.start()
